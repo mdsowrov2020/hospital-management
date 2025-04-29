f61460fa05d90df5d4a4144ffc70d030bf4bf683
Md Sowrov <antukhadem17@gmail.com>
2025-04-29 19:54:50 +0600

    Validators created

---
+++ backend/utils/validators.js
@@ -0,0 +1,22 @@
+import body from "express-validator";
+
+export const validateRegister = [
+  body("firstName").notEmpty().withMessage("First name is required"),
+  body("lastName").notEmpty().withMessage("Last name is required"),
+  body("email").notEmpty().withMessage("Valid email is required"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+];
+export const validateLogin = [
+  body("email").isEmail().withMessage("Valid email is required"),
+  body("password").notEmpty().withMessage("Password is required"),
+];
+export const validateUpdateProfile = [
+  body("firstName").optional().notEmpty().withMessage("First name is required"),
+  body("lastName").optional().notEmpty().withMessage("Last name is required"),
+  body("password")
+    .optional()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+];
