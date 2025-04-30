d9c442f925d4dab46107960682721f9af4a13466
Md Sowrov <antukhadem17@gmail.com>
2025-04-30 11:41:00 +0600

    Created patient type

--- frontend/lib/api/patients/types.ts
+++ frontend/lib/api/patients/types.ts
@@ -1,50 +1,20 @@
-import { Dayjs } from "dayjs";
-
-export type Gender = "male" | "female" | "other";
+import { User } from "../users/type";
 
 export interface Patient {
   id: number;
   userId: number;
   dateOfBirth: string;
-  gender: Gender;
+  gender: "male" | "female" | "other";
   bloodType?: string;
   address?: string;
   phoneNumber: string;
-  createdAt?: string;
-  updatedAt?: string;
+  user?: User;
 }
 
-export interface CreatePatientDto {
+export interface CreatePatientData {
   userId: number;
-  dateOfBirth: string | Dayjs;
-  gender: Gender;
-  bloodType?: string;
-  address?: string;
-  phoneNumber: string;
-}
-
-export interface UpdatePatientDto {
-  dateOfBirth?: string | Dayjs;
-  gender?: Gender;
-  bloodType?: string | null;
-  address?: string | null;
-  phoneNumber?: string;
-}
-
-export interface PatientFilter {
-  userId?: number;
-  gender?: Gender;
-  bloodType?: string;
-  dateRange?: {
-    start: string;
-    end: string;
-  };
-}
-
-export interface PatientFormValues {
-  userId: number;
-  dateOfBirth: Dayjs;
-  gender: Gender;
+  dateOfBirth: string;
+  gender: "male" | "female" | "other";
   bloodType?: string;
   address?: string;
   phoneNumber: string;
